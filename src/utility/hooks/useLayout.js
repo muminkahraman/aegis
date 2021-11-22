0e61a25838427a7b0daf7682fb2ea83a1b46e1d8
muminkahraman <kahraman.mumin.10@gmail.com>
2021-11-22 14:25:47 +0100

    ajout nouveaux menus

--- src/utility/hooks/useLayout.js
+++ src/utility/hooks/useLayout.js
@@ -23,15 +23,15 @@ export const useLayout = () => {
       // ** Allow value to be a function so we have same API as useState
       const valueToStore = value instanceof Function ? value(layout) : value
 
-      // ** Set state
+       // ** Set state
       setLayout(valueToStore)
     } catch (error) {
-      // ** A more advanced implementation would handle the error case
+       // ** A more advanced implementation would handle the error case
       console.log(error)
     }
   }
 
-  const handleLayout = () => {
+  const handleLayout = () => {  
     // ** If layout is horizontal & screen size is equals to or below 1200
     if (layout === 'horizontal' && window.innerWidth <= 1200) {
       setLayout('vertical')
